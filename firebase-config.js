ddc7d971912f5ac352cd1b8067ee8c3fc4873b58
Amey T <48093400+L4WLI3T@users.noreply.github.com>
2026-06-28 00:11:56 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Paste your Firebase project's config here.
+// Firebase Console -> Project settings -> General -> Your apps -> SDK setup and configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBs_yo6jIArWfvBxmMpBNnTeMUc31CqbfA",
+  authDomain: "exp-tracker-39c47.firebaseapp.com",
+  projectId: "exp-tracker-39c47",
+  storageBucket: "exp-tracker-39c47.firebasestorage.app",
+  messagingSenderId: "1045626003690",
+  appId: "1:1045626003690:web:d84378d74abc8c8b32f793",
+  measurementId: "G-H7J9LC85X1"
+};
